2f647101a71081f84ee32a8fd8cee2777585dc2c
bkane <bbkanego@gmail.com>
2026-01-18 23:10:08 -0500

    First commit

--- adk_db_agent/agent.py
+++ adk_db_agent/agent.py
@@ -4,16 +4,16 @@ from google.adk.models.lite_llm import LiteLlm
 from utils.file_utils import read_file
 from dbscripts import database
 import pandas as pd
-from pandas import DataFrame
 
 model = LiteLlm(
     model="ollama_chat/llama3.1:8b"
+    #model="ollama_chat/qwen3:latest"
 )
 
 sql_engine = database.get_engine()
 
 
-def run_sql_query(query: str) -> DataFrame:
+def run_sql_query(query: str) -> list:
     """Returns dataframe of the results after running the SQL query against the PostgreSQL.
 
     Args:
@@ -22,7 +22,12 @@ def run_sql_query(query: str) -> DataFrame:
     Returns:
         dataframe: dataframe of the results
     """
-    return pd.read_sql(query, con=sql_engine)
+    print(f'>>>>> The sql engine was called={query} and SQL engine is {sql_engine}')
+    df = pd.read_sql(query, con=sql_engine)
+    response_list = df.to_dict(orient='records')
+    print(f'Response List {response_list}')
+    return response_list
+
 
 
 instruction_prompt = read_file('prompt/emp_system_prompt.txt')
